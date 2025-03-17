066e8c267e48a7f4534ee0c044caa8d926cec191
mjb90 <manasborah90@gmail.com>
2025-03-17 22:09:41 +0800

    mock tests

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,14 +6,14 @@ import {FundMe} from "../src/FundMe.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
-contract DeployFundMe is Script{
-    function run() external returns (FundMe){
-       HelperConfig helperConfig = new HelperConfig();
-       address priceFeedAddress = helperConfig.activeEthConfig();
-       vm.startBroadcast();
-       FundMe fundMe = new FundMe(priceFeedAddress);
-       vm.stopBroadcast();
-       console.log(address(this));
-       return fundMe;
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address priceFeedAddress = helperConfig.activeEthConfig();
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(priceFeedAddress);
+        vm.stopBroadcast();
+        console.log(address(this));
+        return fundMe;
     }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,23 +3,35 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
-contract HelperConfig{
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
     NetworkConfig public activeEthConfig;
-    struct NetworkConfig{
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_ANSWER = 2000e8;
+    struct NetworkConfig {
         address priceFeed;
     }
 
-    constructor(){
-        if(block.chainid == 11155111)
-            activeEthConfig = getSepoliaEthConfig();
-        else activeEthConfig = getAnvilEthConfig();
+    constructor() {
+        if (block.chainid == 11155111) activeEthConfig = getSepoliaEthConfig();
+        else activeEthConfig = getOrCreateAnvilEthConfig();
     }
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory){
-        return NetworkConfig(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeEthConfig.priceFeed != address(0)) return activeEthConfig;
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_ANSWER
+        );
+        vm.stopBroadcast();
 
+        return NetworkConfig(address(mockPriceFeed));
     }
 }
